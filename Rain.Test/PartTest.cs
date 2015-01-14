f61fef501821a4c5a21b526b450ad98fa738843f
Martin Brennan <mjrbrennan@gmail.com>
2015-01-14 12:44:00 +1000

    Added part tests and methods

---
+++ Rain.Test/PartTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Rain.Test
+{
+    [TestClass]
+    public class PartTest
+    {
+        public Rain.Part part;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            this.part = new Rain.Part();
+        }
+
+        [TestMethod]
+        public void ItInitializesWithDefaultValues()
+        {
+            Assert.AreEqual(null, part.HttpMethod);
+            Assert.AreEqual(0, part.Responses.Count);
+            Assert.AreEqual(0, part.Docs.Count);
+            Assert.AreEqual(null, part.Route);
+            Assert.AreEqual(0, part.Headers.Count);
+            Assert.AreEqual(0, part.Parameters.Count);
+        }
+
+        [TestMethod]
+        public void ItCanAppendDocsToTheSpecifiedResponseCode()
+        {
+            part.AppendResponse(200, "success", "{");
+            part.AppendResponse(200, "success", "  id: 1490");
+            part.AppendResponse(200, "success", "}");
+            Assert.AreEqual("{  id: 1490}", part.GetResponse(200, "success").Replace("\n", ""));
+        }
+    }
+}
--- Rain/Part.cs
+++ Rain/Part.cs
@@ -8,6 +8,85 @@ namespace Rain
 {
     public class Part
     {
+        public string HttpMethod { get; set; }
+        public List<Response> Responses { get; set; }
+        public List<String> Docs { get; set; }
+        public string Route { get; set; }
+        public List<Header> Headers { get; set; }
+        public List<Parameter> Parameters { get; set; }
 
+        /// <summary>
+        /// Set up property values.
+        /// </summary>
+        public Part()
+        {
+            this.Responses = new List<Response>();
+            this.Docs = new List<string>();
+            this.Headers = new List<Header>();
+            this.Parameters = new List<Parameter>();
+        }
+
+        /// <summary>
+        /// Appends a snippet of text to an existing response
+        /// example or adds a new response example with the
+        /// specified text.
+        /// </summary>
+        /// <param name="code">The response code to use e.g. 200, 400</param>
+        /// <param name="id">The unique id of the response e.g. success, bad_param. This is so
+        /// you can have multiple response code examples, for example different validation errors
+        /// will return different 400 responses.</param>
+        /// <param name="text">The text of the response, used as the example.</param>
+        public void AppendResponse(int code, string id, string text)
+        {
+            List<Response> responses = this.Responses.Where(r => r.Code == code).ToList();
+            if (responses.Count == 0)
+            {
+                this.Responses.Add(new Response()
+                {
+                    Id = id,
+                    Code = code,
+                    Text = new List<string>() { text }
+                });
+            }
+            else
+            {
+                Response response = responses.Where(r => r.Id == id).First();
+                response.Text.Add(text);
+            }
+        }
+
+        /// <summary>
+        /// Gets a response's text joined with newlines
+        /// based on the unique response code and id.
+        /// </summary>
+        /// <param name="code">The response code to search for.</param>
+        /// <param name="id">The id of the response to search for.</param>
+        /// <returns>All of the response text joined with newlines.</returns>
+        public string GetResponse(int code, string id)
+        {
+            Response response = Responses.Where(r => r.Id == id && r.Code == code).First();
+            return String.Join("\n", response.Text.ToArray());
+        }
+
+        public class Response
+        {
+            public int Code { get; set; }
+            public string Id { get; set; }
+            public List<String> Text { get; set; }
+        }
+
+        public class Parameter
+        {
+            public string Name { get; set; }
+            public List<String> Text { get; set; }
+            public string Type { get; set; }
+            public string Default { get; set; }
+        }
+
+        public class Header
+        {
+            public string Name { get; set; }
+            public List<String> Text { get; set; }
+        }
     }
 }
